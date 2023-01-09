8cf8e938d7b347bf46a91eba38c8d547faad27ef
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2023-01-09 13:30:03 -0300

    Exercicio 10

---
+++ Exercicios-Funções/Ex10.js
@@ -0,0 +1,18 @@
+/*
+    10) Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+    ou false
+*/
+
+function divisivelPor3(numero){
+    if(numero % 3 === 0){
+        return true
+    }else{
+        return false
+    } 
+}
+
+console.log(divisivelPor3(3)) 
+console.log(divisivelPor3(4))
+console.log(divisivelPor3(16))
+console.log(divisivelPor3(30))
+console.log(divisivelPor3(44))
