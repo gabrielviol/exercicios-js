c6dbebbae1420919fd99d6427d11ceb7231d4097
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2023-01-10 14:05:53 -0300

    Exercicio 24

---
+++ Exercicios-Funções/Ex24.js
@@ -0,0 +1,13 @@
+/*
+    24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+*/
+
+function Hello(){
+    let i = 1
+    while(i < 12){
+        console.log("Hello World!", i)
+        i++
+    }
+}
+
+Hello()
