bc14032fb640cb9bb4036ad0a6af913801dfb4fc
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2023-01-12 14:35:42 -0300

    Exercicio 40 - FIM

--- Exercicios-Funções/Ex40.js
+++ Exercicios-Funções/Ex40.js
@@ -2,3 +2,23 @@
 // modo que de 0,0 a 4,9 seja atribuído o conceito D, de 5,0 a 6,9 seja atribuído o conceito C, de 7,0 a 8,9 o
 // conceito B e de 9,0 a 10,0 o conceito A.
 
+vetor = [ 1, 6.9, 7, 10]
+
+function conceitoNotas(vetor){
+    let vetorFinal = vetor.map(nota => {
+        if( nota < 5){
+            return [nota, 'D']
+        } else if(nota < 7){
+            return [nota, 'C']
+        } else if(nota < 9){
+            return [nota, 'B']
+        } else if(nota <= 10){
+            return [nota, 'A']
+        } else{
+            return 'Erro'
+        }
+    })
+    return vetorFinal
+}
+
+console.log(conceitoNotas(vetor))
