a7319ce55df6c4e6becc5bb2b46cb7789b1efc36
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2023-01-24 19:19:20 -0300

    add folder array

---
+++ Array/arrayMetodos.js
@@ -0,0 +1,9 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() //remove o ultimo elemento
+console.log(pilotos)
+
+pilotos.push('Vertappen') //Adiciona elemento ao final do array
+console.log(pilotos)
+
+pilotos.shift() //remove primeiro elemento da lista
+console.log(pilotos)
