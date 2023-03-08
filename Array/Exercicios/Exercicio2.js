57c37b055ec5b66c516af66a2c3b871e4e174225
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2023-03-08 20:51:30 -0300

    exercicio 2

---
+++ Array/Exercicios/Exercicio2.js
@@ -0,0 +1,10 @@
+/**
+ * Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+ */
+
+function idadeEmDias(idade){
+    let dias = idade * 365;
+    return console.log(dias);
+}
+
+idadeEmDias(25)
