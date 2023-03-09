5c8042575a0da91fb58e0613e48b3760eee7854d
Gabriel Oliveira <gabrielviol00@hotmail.com>
2023-03-09 16:50:21 -0300

    exercicio 4

---
+++ Array/Exercicios/exercicio4.js
@@ -0,0 +1,12 @@
+/**
+    
+ */
+
+function receberNomeDoMes(numero) {
+    const mapeamento = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
+        'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+
+    return console.log(mapeamento[--numero])
+}
+
+receberNomeDoMes(12)
