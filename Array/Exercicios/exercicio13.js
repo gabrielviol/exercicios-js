d0aed9b602cd1a3f26ba6d0f3f8ed4c722f0d610
Gabriel Oliveira <gabrielviol00@hotmail.com>
2023-03-10 17:29:43 -0300

    exercicio13.js

---
+++ Array/Exercicios/exercicio13.js
@@ -0,0 +1,10 @@
+/**
+    Crie uma função que receba um array de elementos e retorne um array somente com os números presentes no
+    array recebido como parâmetro.
+ */
+
+function retornaNumber(array){
+    return array.filter(item => typeof item === "number")
+}
+
+console.log(retornaNumber(['oi','oi',2,3,4,'oi','oi',1,true]))
