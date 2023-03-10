ce94dbdff00e76c53199757ad8273fbb526649c8
Gabriel Oliveira <gabrielviol00@hotmail.com>
2023-03-10 15:03:53 -0300

    exercicio 10

---
+++ Array/Exercicios/exercicio10.js
@@ -0,0 +1,11 @@
+/**
+    Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na
+    quantidade especificada no parâmetro.
+ */
+
+function simboloMais(quantidade) {
+    return "+".repeat(quantidade)
+}
+
+console.log(simboloMais(2))
+        
